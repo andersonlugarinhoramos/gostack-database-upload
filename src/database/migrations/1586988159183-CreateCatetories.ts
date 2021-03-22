0253ac8e7135f14468122f7b238790ac6b95ad22
Anderson <anderson@sistecweb.com.br>
2021-03-22 17:48:57 -0300

    # correction 01

--- src/database/migrations/1586988159183-CreateCatetories.ts
+++ src/database/migrations/1586988159183-CreateCatetories.ts
@@ -1,15 +1,15 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateTransactions1589758849285
+export default class CreateCatetories1586988159183
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'transactions',
+        name: 'categories',
         columns: [
           {
             name: 'id',
-            type: 'uuid ',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -17,16 +17,6 @@ export default class CreateTransactions1589758849285
           {
             name: 'title',
             type: 'varchar',
-          },
-          {
-            name: 'type',
-            type: 'varchar',
-          },
-          {
-            name: 'value',
-            type: 'decimal',
-            precision: 10,
-            scale: 2,
           },
           {
             name: 'created_at',
@@ -44,6 +34,6 @@ export default class CreateTransactions1589758849285
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('transactions');
+    await queryRunner.dropTable('categories');
   }
 }
